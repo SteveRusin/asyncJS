63e91fe1523aff41cb0f57536c310d609a13b383
Stepan Sendun <Stepan_Sendun@epam.com>
2018-01-23 13:02:52 +0200

    finished task2

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const ul = document.querySelector('.list-group');
 const tasks = [];
-for (let i=1; i<=10; i++){
-    tasks.push(`https://jsonplaceholder.typicode.com/users/${i}`);
+for (let i = 1; i <= 10; i++) {
+    tasks.push(fetch(`https://jsonplaceholder.typicode.com/users/${i}`));
 }
 
-function  getInSeries() {
-    let sequence = Promise.resolve();
-   return tasks.map(task=>{
-       return sequence = sequence.then(()=>{
-            return fetch(task)
-                .then(res=>res.json())
-                .then(user=>showUserInfo(user));
+function getInParallel() {
+    return Promise.all(tasks)
+        .then(responses=>{
+            return Promise.all(responses.map(res =>res.json()));
         });
-    })
 }
+
 
 function showUserInfo({id, name}) {
     ul.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">${name}
                     <span class="badge badge-primary badge-pill">${id}</span></li>`;
 }
 
-getInSeries();
+
+
+getInParallel().then(res => res.forEach(user=>showUserInfo(user)));
 
 
 
