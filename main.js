10f22af37656b272df31e741f50f331e534d9712
Stepan Sendun <Stepan_Sendun@epam.com>
2018-01-23 14:45:49 +0200

    some corrections

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
 const ul = document.querySelector('.list-group');
-const container = document.querySelector('.container');
 const modal = document.getElementById('myModal');
 const span = document.querySelector('span.close');
 const tasks = [];
-span.onclick = function() {
-    modal.style.display = "none";
-}
+span.addEventListener('click', ()=>modal.style.display = "none");
 
-for (let i=1; i<=10; i++){
+for (let i = 1; i <= 10; i++) {
     tasks.push(`https://jsonplaceholder.typicode.com/users/${i}`);
 }
 
-let timeOut = new Promise((res, rej)=>{
-    let wait = setTimeout(()=>{
+let timeOut = new Promise((res, rej) => {
+    let wait = setTimeout(() => {
         clearTimeout(wait);
         rej();
         modal.style.display = "block";
     }, 2000)
 });
 
-function  getInSeries() {
+function getInSeries() {
     let sequence = Promise.resolve();
-   let tempArr = tasks.map(task=>{
-       return sequence = sequence.then(()=>{
+    return tasks.map(task => {
+        return sequence = sequence.then(() => {
             return Promise.race([fetch(task), timeOut])
-                .then((res, rej) => res.json())
-                .then(user=>showUserInfo(user));
+                .then(res => res.json())
+                .then(user => showUserInfo(user));
         });
     }).concat(timeOut);
 }
