6dec3c5da1eab0db93f1422a1ec12f826c6d400e
Stepan Sendun <Stepan_Sendun@epam.com>
2018-01-26 16:04:13 +0200

    rewrite using reduce

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const ul = document.querySelector('.list-group');
 const tasks = [];
-for (let i=1; i<=10; i++){
+for (let i = 1; i <= 10; i++) {
     tasks.push(`https://jsonplaceholder.typicode.com/users/${i}`);
 }
 
-function  getInSeries() {
-    let sequence = Promise.resolve();
-   return tasks.map(task=>{
-       return sequence = sequence.then(()=>{
-            return fetch(task)
-                .then(res=>res.json())
-                .then(user=>showUserInfo(user));
-        });
-    })
+function getInSeries() {
+    return tasks.reduce((promise, url) => {
+        return promise.then(()=>{
+            return fetch(url)
+                .then(data=>data.json())
+                .then(user=>showUserInfo(user))
+        })
+    }, Promise.resolve())
 }
 
 function showUserInfo({id, name}) {
@@ -21,10 +20,3 @@ function showUserInfo({id, name}) {
 }
 
 getInSeries();
-
-
-
-
-
-
-
